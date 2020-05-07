aa8619f38aa28c7e8d282d04348e0180ab0cff30
twoheart <twoheart@vip.qq.com>
2020-05-07 23:56:01 +0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+  },
+  plugins: [new HtmlWebpackPlugin()],
+};
